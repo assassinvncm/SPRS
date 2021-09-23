3b2b5a2adcb1a04e5eef34a5fff60b0a33790e58
assassinvncm <huntercity00000@gmail.com>
2021-09-23 23:19:41 +0700

    first commit

---
+++ src/main/java/com/SprsApplication.java
@@ -0,0 +1,13 @@
+package com;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SprsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SprsApplication.class, args);
+	}
+
+}
