eff16a922766400f6414adcfa223e156aa1b3c34
assassinvncm <huntercity00000@gmail.com>
2021-10-07 00:35:06 +0700

    otp sending and validate using jwtToken

--- src/main/java/com/ultils/Constants.java
+++ src/main/java/com/ultils/Constants.java
@@ -6,5 +6,6 @@ public class Constants {
 	public static final String SUCCESS = "101";
 	public static final String FAILED = "102";
 	public static final String SERVER_ERR = "501";
-	public static final String[] NONE_AUTH = {"/authenticate", "/sprs/api/user", "/sprs/api/group", "/sprs/api/group/{id}"};
+	public static final String OTP_MESSAGE = "Mã xác minh SPRS của bạn là ";
+	public static final String[] NONE_AUTH = {"/authenticate", "/sprs/api/user", "/sprs/api/group", "/sprs/api/group/{id}"};//, "/sprs/api/generateOtp", "/sprs/api/validateOtp"
 }
