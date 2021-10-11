4df2502a7da09e1f460632668b0ec772fd14eee6
assassinvncm <huntercity00000@gmail.com>
2021-10-11 22:57:20 +0700

    cm ao ma canada

--- src/main/java/com/api/service/SmsService.java
+++ src/main/java/com/api/service/SmsService.java
@@ -7,21 +7,16 @@ import com.api.entity.SmsPojo;
 import com.twilio.Twilio;
 import com.twilio.rest.api.v2010.account.Message;
 import com.twilio.type.PhoneNumber;
+import com.ultils.Constants;
 
 @Component
 public class SmsService {
 
-	private final String ACCOUNT_SID = "ACaef9117a42d80b9837a3d4bc0acf8fe0";
-
-	private final String AUTH_TOKEN = "f5cfea6a48189e9e06cc18dce99e0dbf";
-
-	private final String FROM_NUMBER = "+14128967877";
-
 	public void send(SmsPojo sms) {
 		try {
-			Twilio.init(ACCOUNT_SID, AUTH_TOKEN);
+			Twilio.init(Constants.ACCOUNT_SID, Constants.AUTH_TOKEN);
 
-			Message message = Message.creator(new PhoneNumber(sms.getTo()), new PhoneNumber(FROM_NUMBER), sms.getMessage()).create();
+			Message message = Message.creator(new PhoneNumber(sms.getTo()), new PhoneNumber(Constants.FROM_NUMBER), sms.getMessage()).create();
 			System.out.println("Here is my id:" + message.getSid());// Unique resource ID created to manage this transaction
 		} catch (Exception e) {
 			e.printStackTrace();
