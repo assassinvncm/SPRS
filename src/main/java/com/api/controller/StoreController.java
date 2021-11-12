63952ee1118a8d1338e21413954358e56841dbe5
assassinvncm <huntercity00000@gmail.com>
2021-11-12 22:22:20 +0700

    img rv

--- src/main/java/com/api/controller/StoreController.java
+++ src/main/java/com/api/controller/StoreController.java
@@ -88,16 +88,19 @@ public class StoreController {
 		Store s = structMapper.storeDtoToStore(storeDto);
 		storeService.updateStore(s);
 		logger.info("End update Store");
-		return ResponseEntity.ok(new SPRSResponse(Constants.SUCCESS, "Update Store By ID "+s.getId()+" success", "", s, null));
+		return ResponseEntity.ok(new SPRSResponse(Constants.SUCCESS, "Update Store By ID "+storeDto.getId()+" success", "", storeDto, null));
 	}
 
-	@RequestMapping(value = "/uploadImg", method = RequestMethod.POST)
-	public ResponseEntity<?> uploadImg(@RequestPart(value = "file") MultipartFile file, @RequestBody StoreDto storeDto) {
+	@RequestMapping(value = "/uploadImg/{id}", method = RequestMethod.PUT)
+	public ResponseEntity<?> uploadImg(@RequestPart(value = "file") MultipartFile file, @PathVariable(value = "id") Long id) {
 		logger.info("Start uploadImg Store");
-		Store s = structMapper.storeDtoToStore(storeDto);
+		Store st = storeRepository.getById(id);
+		if(null == st) {
+			throw new AppException(402,"Store is not Found!");
+		}
 		String img_url = amazonClient.uploadFile(file);
-		storeService.updateStoreImg(s,img_url);
+		storeService.updateStoreImg(st,img_url);
 		logger.info("End uploadImg Store");
-		return ResponseEntity.ok(new SPRSResponse(Constants.SUCCESS, "Update Store By ID "+s.getId()+" success", "", s, null));
+		return ResponseEntity.ok(new SPRSResponse(Constants.SUCCESS, "Update Store By ID "+st.getId()+" success", "", st, null));
 	}
 }
